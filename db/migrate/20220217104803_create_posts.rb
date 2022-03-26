90a8eef87a61d4973c2c1b4fe574ee54eea91652
Annah Tomoro <annaht.annah@gmail.com>
2022-03-26 20:04:56 +0300

    Implement required changes

--- db/migrate/20220217104803_create_posts.rb
+++ db/migrate/20220217104803_create_posts.rb
@@ -1,12 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :Post do |t|
-      t.references :User, null: false, foriegn_key: true, index:true
-      t.string :Title
-      t.string :Text
+      t.references :author, foreign_key: { to_table: 'users' }
+      t.string :title
+      t.string :text
       t.timestamps
-      t.integer :Comments_Counter ,default: 0,null: false
-      t.integer :Likes_Counter ,default: 0,null: false
+      t.integer :comments_counter ,default: 0,null: false
+      t.integer :likes_counter ,default: 0,null: false
     end
   end
 end
--- db/migrate/20220217104818_create_likes.rb
+++ db/migrate/20220217104818_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :Like do |t|
-      t.references :User, null: false, foriegn_key: true, index:true
-      t.references :Post, null: false, foriegn_key: true, index:true
+      t.references :author, foreign_key: { to_table: 'users' } 
+      t.references :post, null: false, foriegn_key: true, index:true
       t.timestamps
   end
 end
