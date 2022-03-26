68b2feeaf238d70c6be21fc82bd758244d7fde25
Annah Tomoro <annaht.annah@gmail.com>
2022-03-26 20:25:06 +0300

    update create_users.rb

--- db/migrate/20220217104747_create_users.rb
+++ db/migrate/20220217104747_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :User do |t|
-      t.string :Name
-      t.string :Photo
-      t.text :Bio
+      t.string :name
+      t.string :photo
+      t.text :bio
       t.timestamps
       t.integer :Posts_Counter ,default: 0,null: false
   end
